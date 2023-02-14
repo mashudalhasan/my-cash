0d63c36143cff343d6cd3534b7e103592cba5f13
mashudalhasan <mashudalhasan554@gmail.com>
2023-02-14 12:46:48 +0600

    second commit

--- js/deposit.js
+++ js/deposit.js
@@ -2,6 +2,13 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     const depositField = document.getElementById('deposit-field');
     const newDepositAmountString = depositField.value;
     const newDepositAmount = parseFloat(newDepositAmountString);
+    
+    depositField.value = '';
+    
+    if(isNaN(newDepositAmount)){
+        alert('Please provide a valid number');
+        return;
+    }
 
     const depositTotalElement = document.getElementById('deposit-total');
     const previousTotalAmountString = depositTotalElement.innerText;
@@ -18,5 +25,4 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
     balanceTotalElement.innerText = currentBalanceTotal;
 
-    depositField.value = '';
 })
